c5b3f7ddede01945cc79bd66689f5cbb9da19c41
ialeksandrovi <ialexandrov@dejavoosystems.ru>
2021-03-25 23:44:45 +0700

    first commit

---
+++ app/src/main/java/com/axwel/personal_notes/DefaultNote.kt
@@ -0,0 +1,10 @@
+package com.axwel.personal_notes
+
+import java.util.*
+
+class DefaultNote: Note {
+    var title: String = ""
+    var message: String = ""
+    var dateCreation: Date? = null
+    var dateLastUpdate: Date? = null
+}
--- app/src/main/java/com/axwel/personal_notes/EditNoteFragment.kt
+++ app/src/main/java/com/axwel/personal_notes/EditNoteFragment.kt
@@ -3,4 +3,11 @@ package com.axwel.personal_notes
 import androidx.fragment.app.Fragment
 
 class EditNoteFragment: Fragment() {
+
+    companion object {
+        val TAG = EditNoteFragment::class.java.simpleName
+        fun newInstance(): EditNoteFragment {
+            return EditNoteFragment()
+        }
+    }
 }
